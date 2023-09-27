584964067904b843728af09b9020675c309e60be
kaylan <kaylan.rosa@proderj.rj.gov.br>
2023-09-26 22:53:38 -0300

    alterações classe

--- Main.py
+++ Main.py
@@ -1,4 +1,17 @@
 from Node import Node
-frase = 'fdsgsfgfdsgtrsythbhftshbshhsh'
-Node.printNodes(Node.montarArvore(frase)[0])
-input()
+
+frase = 'testando como funciona'
+
+arvore_huffman = Node.montarNodes(frase)
+
+codigos = Node.gerarCodigos(arvore_huffman[0])
+
+frase_codificada = ""
+for caractere in frase:
+    frase_codificada += codigos[caractere]
+
+with open("codificada.txt", "wb") as arquivo:
+    # arquivo.write(bytes(frase_codificada, encoding="utf-8"))
+    arquivo.write(frase_codificada.encode("utf-8"))
+
+print("Frase codificada:", frase_codificada)
--- Node.py
+++ Node.py
@@ -1,46 +1,52 @@
 class Node:
-    def __init__(self, frequencia, caractere, folhaEsquerda = None, folhaDireita = None):
+    def __init__(self, frequencia, caractere, esquerda=None, direita=None):
         self.frequencia = frequencia
         self.caractere = caractere
-        self.folhaEsquerda = folhaEsquerda
-        self.folhaDireita = folhaDireita
+        self.esquerda = esquerda
+        self.direita = direita
         self.codigo = ''
 
     @staticmethod
-    def printNodes(node, codigo = ''):
-        codigoConcatenado = codigo + str(node.codigo)
-        if(node.folhaEsquerda):
-            Node.printNodes(node.folhaEsquerda, codigoConcatenado)
-        if(node.folhaDireita):
-            Node.printNodes(node.folhaDireita, codigoConcatenado)
-        if(not node.folhaEsquerda and not node.folhaDireita):
-            print(f"{node.caractere} -> {codigoConcatenado}")
-            
+    def gerarCodigos(node, codigo=''):
+        codigoConcatenado = codigo + node.codigo
+        caracteresECodigos = {}  
+        if node.esquerda:
+            caracteresECodigos.update(Node.gerarCodigos(node.esquerda, codigoConcatenado))
+        if node.direita:
+            caracteresECodigos.update(Node.gerarCodigos(node.direita, codigoConcatenado))
+        if not node.esquerda and not node.direita:
+            caracteresECodigos[node.caractere] = codigoConcatenado
+        return caracteresECodigos
+    
     @staticmethod
-    def montarArvore(frase):
-        arrayLetras = {}
+    def montarNodes(frase):
+        frequenciaCaracteres = {}
         arrayNode = []
 
-        for i in frase:
-            if i not in arrayLetras:
-                arrayLetras[i] = 1
+        for caractere in frase:
+            if caractere not in frequenciaCaracteres:
+                frequenciaCaracteres[caractere] = 1
             else:
-                arrayLetras[i]+=1
-        arrayLetras = {k: v for k, v in sorted(arrayLetras.items(), key=lambda item: item[1])}
+                frequenciaCaracteres[caractere] += 1
 
+        frequenciaCaracteres = {k: v for k, v in sorted(frequenciaCaracteres.items(), key=lambda item: item[1])}
 
-        for i in range (0, len(arrayLetras)):
-            arrayNode.append(Node(arrayLetras[list(arrayLetras.keys())[i]], list(arrayLetras.keys())[i]))
-    
-        while (len(arrayNode)>1):
-            node = Node(arrayNode[0].frequencia + arrayNode[1].frequencia, arrayNode[0].caractere + arrayNode[1].caractere, arrayNode[0], arrayNode[1])
-            arrayNode[0].codigo = 0
-            arrayNode[1].codigo = 1
-            for i in range(len(arrayNode)-1, 0, -1):
-                if(node.frequencia >= arrayNode[i].frequencia):
+        for i in range(0, len(frequenciaCaracteres)):
+            arrayNode.append(Node(frequenciaCaracteres[list(frequenciaCaracteres.keys())[i]], list(frequenciaCaracteres.keys())[i]))
+
+        while len(arrayNode) > 1:
+            node = Node(arrayNode[0].frequencia + arrayNode[1].frequencia,
+                        arrayNode[0].caractere + arrayNode[1].caractere,
+                        arrayNode[0], arrayNode[1])
+            arrayNode[0].codigo = '0'
+            arrayNode[1].codigo = '1'
+            for i in range(len(arrayNode) - 1, 0, -1):
+                if node.frequencia >= arrayNode[i].frequencia:
                     arrayNode.insert(i + 1, node)
                     break
             arrayNode.pop(0)
             arrayNode.pop(0)
 
         return arrayNode
+
+
